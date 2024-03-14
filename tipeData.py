2c4585336f095dd00908e58d207a4adfa20969f2
MI1412 <imronm1309@gmail.com>
2024-03-14 12:16:21 +0700

    tipe data py

---
+++ tipeData.py
@@ -0,0 +1,25 @@
+# tipe data 
+# integer 
+tipeInteger = 9 
+print ("data : ", tipeInteger, "tipe data : ", type(tipeInteger))
+
+#float
+tipeFloat = 1.8
+print ("data : ", tipeFloat, "tipe data : " , type(tipeFloat))
+
+# string 
+tipeString = "imron"
+print("data : ", tipeString, "tipe data : ", type(tipeString))
+
+#boolean adalah bertipe binary
+tipeBoolean = True
+print("data : ", tipeBoolean, "tipe data : ", type(tipeBoolean))
+
+# tipe data khusus complex
+dataKomplex = complex(5,6)
+print("data : ", dataKomplex, "tipe data : ", type(dataKomplex))
+
+# tipe data dari bahasa C
+from ctypes import c_double
+dataCDouble = c_double(10.5)
+print("data : ", dataCDouble, "tipe data : ", type(dataCDouble))
