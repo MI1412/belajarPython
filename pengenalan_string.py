d1e8373d6127d8daef9081c24da3e8d4b579c7ec
MI1412 <imronm1309@gmail.com>
2024-03-17 13:51:19 +0700

    pengenalan string

---
+++ pengenalan_string.py
@@ -0,0 +1,19 @@
+# string digunakan untuk menampilkan teks dengan sintaks '',""
+# karakter khusus bisa digunakan dengan \ atau bisa menjadikan string
+# contoh : 
+# \n
+print("ini \nbaru dienter")
+print("ini \bdihapus")
+# contoh :
+data = "halo ini imron"
+print(data,"tipe data",type(data))
+print("ini adalah hari jum\'at")
+
+# menggunakan raw string cuku tambahkan r didepan string / raw string
+print(r"c:\newfolder")
+
+# multiline string
+print("""
+      nama : imron
+      kelas : 1 smk
+      """)
