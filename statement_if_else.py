4852568ededcc0b1e6bc96a2e96aac18cf2ad03f
MI1412 <imronm1309@gmail.com>
2024-03-20 16:35:46 +0700

    statement if else python

---
+++ statement_if_else.py
@@ -0,0 +1,11 @@
+# statement if else
+nama = input("siapa nama anda \t: ")
+
+# if else di python menggunakan indentasi atau spasi jadi perhatikan spasinya
+if nama == "imron":
+    print("halo imron")
+# keluar dari program dengan backspace
+# else statement
+else:
+    print("kamu bukan pembuat saya")    
+print(f"terima kasih {nama}") 
