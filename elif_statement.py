0f67c092fcda4f2ac7239ccc3e0c23590f6f5661
MI1412 <imronm1309@gmail.com>
2024-03-20 16:49:47 +0700

    statement elif python

---
+++ elif_statement.py
@@ -0,0 +1,15 @@
+# elif = else if
+# digunakan untuk memberi kondisi tambahan 
+nama = input("nama kamu siapa : ")
+
+if nama == "imron":
+    print("halo pengembang")
+    # kondisi 1
+elif nama == "musa":
+    print("halo ustad")
+    # kondisi 2
+elif nama == "mama":
+    print("halo mama")    
+else:
+    print(f"maaf saya tidak mengenal {nama}")        
+    
