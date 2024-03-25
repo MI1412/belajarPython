815373c746ae977f05e039981d3f7a3ca0c37c64
MI1412 <imronm1309@gmail.com>
2024-03-25 16:48:05 +0700

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+# list -> array, mengakses data menggunakan index
+data_list = ["imr","ms","ha"]
+print(data_list[1])
+
+# dictionary (dict) adalah asosiative array
+# identitas = key
+
+data_dict = {
+    'key':'value',
+    'imr':'imron',
+    'ms':'musa'
+}
+
+print(data_dict['key'])
+print(data_dict['imr'])
