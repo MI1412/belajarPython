11609b5600b29f68c741bfe711209a74ef869a7b
MI1412 <imronm1309@gmail.com>
2024-03-25 16:40:28 +0700

    tupples dan sets

---
+++ tuples_dan_sets.py
@@ -0,0 +1,17 @@
+# tuples dan sets
+# list data yang bisa dirubah
+data_list = [10,2,4,5,1,1]
+print(data_list)
+
+# tuples data yang tidak bisa dirubah isinya
+data_tuples = (1,2,3,4,5,6,7)
+print(data_tuples)
+print(data_tuples[1])
+
+# data_tuples[1] = "imron" ini tidak bisa dijalankan 
+
+# sets tidak bisa memakai index atau disebut himpunan
+data_sets = {1,2,3,4,5,6}
+print(data_sets)
+
+# print(data_sets[0])
