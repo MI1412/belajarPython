83dd2088333b73f376d75940609bf72b9c19bcc9
MI1412 <imronm1309@gmail.com>
2024-03-27 21:55:21 +0700

    function di python

---
+++ function.py
@@ -0,0 +1,16 @@
+# function adalah sebuah fungsi yang digunakan untuk membuat fungsi agar tidak ada  penulisan kode berulang
+# function di python 
+# def artinya definition untuk function
+def halo_dunia():
+    # membuat function untuk menampilkan halo dunia
+    print('halo dunia')
+    print('halo imron')
+
+halo_dunia()
+
+# function() ini akan eror 
+    # pemanggilan function harus setelah dibuat
+def function():
+    print('ini adalah fungsi')
+    
+function() # berfungsi
