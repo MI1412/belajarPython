a351ebbb7f1d2513cc2ede15a0526543224252a7
MI1412 <imronm1309@gmail.com>
2024-04-18 18:27:40 +0700

    modified : anonymous and lambda funct.py

--- anonymous_and_lambda_funct.py
+++ anonymous_and_lambda_funct.py
@@ -2,7 +2,7 @@
 def kuadrat(angka):
     return angka**2
 
-print(f"hasil fungsi kuadrat = {kuadrat(7)}")
+print(f"hasil fungsi kuadrat 7 = {kuadrat(7)}")
 
 
 # lambda function
@@ -11,16 +11,16 @@ kuadrat = lambda angka:angka**2
 print(f"hasil kuadrat memakai lambda = {kuadrat(9)}")
 
 pangkat = lambda num,pow : num**pow
-print(f"hasil 2 input lambda pangkat = {pangkat(2,4)}")
+print(f"hasil 2 input lambda berpangkat = {pangkat(2,4)}")
 
 # fungsi lambda :
-# 1. sorting list
+# 1. sorting list menurut alphabet
 # general
 data_list = ['imron','musa','amir']
 data_list.sort()
 print(f'\nlist disorted = {data_list}')
 
-# memakai sort list panjang
+# memakai sort list panjang huruf
 def panjang_list(nama):
     return len(nama)
 data_list.sort(key=panjang_list)
@@ -36,7 +36,7 @@ data_angka = [1,2,4,5,6,7,8,9,0]
 def kurang_dari_lima(angka):
     return angka < 5
 data_angka_baru = list(filter(lambda x:x<7,data_angka))
-print(data_angka_baru)
+print(f"angka difilter kurang dari 7 {data_angka_baru}")
 # genap
 data_genap = list(filter(lambda x:(x%2 == 0),data_angka))
 print(f"\ndata genap : {data_genap}")
