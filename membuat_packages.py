73c271adbb7405a76cf7f2e9303a1526f43c0ffe
MI1412 <imronm1309@gmail.com>
2024-04-27 15:00:44 +0700

    modified membuat_packages.py

--- membuat_packages.py
+++ membuat_packages.py
@@ -1,5 +1,22 @@
 # packages digunakan untuk tempat menaruh module
-import sains.matematika
+import time
+t_start = time.time()
 
-hasil_tambah = sains.matematika.tambah(12,456,7)
-print(f"\nhasil tambah : {hasil_tambah}")
+import sains.matematika
+from sains import fisika
+from sains.fisika import gaya
+
+hasil_tambah = sains.matematika.tambah(12,78,7)
+print(f"\nhasil tambah \t\t: {hasil_tambah}")
+
+t_end = time.time()
+
+print(f"waktu eksekusi adalah \t: {t_end - t_start} ") # untuk menghitung berapa lama waktu eksekusi yang didalamnya ada pycache
+
+hasil_gaya = fisika.gaya(90,10)
+
+print(f"gaya adalah {hasil_gaya}")
+
+hasil_gaya = gaya(9,10)
+
+print(f"gaya adalah {hasil_gaya}")
