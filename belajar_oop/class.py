7179696c5427ae4acb9e5ef5c23d1946e0885f03
MI1412 <imronm1309@gmail.com>
2024-05-16 12:35:15 +0700

    class di python

--- belajar_oop/class.py
+++ belajar_oop/class.py
@@ -1,9 +1,16 @@
 class hero:
+    # class variabel adalah variabel didalam class
+    jumlah = 0
+    
     # magic keyword constructor
-    def __init__(self,name,stat,umur):# argumen self memanggil diri sendiri / memanggil hero 
+    def __init__(self,name,stat,umur): # argumen self memanggil diri sendiri / memanggil hero 
+        # contoh instance variabel :
         self.name = name # variabel penampung nama
         self.stat = stat
         self.umur = umur
+        
+        hero.jumlah += 1 # akses class variabel.dengan titik
+        print('tes '+ name)
 
 
 
@@ -11,7 +18,8 @@ class hero:
 hero1 = hero('hehehahha','pelajar',10)
 
 # memanggil semua argumen
-print(hero1)
+print(hero1.__dict__)
+print(hero.jumlah)
 # contoh memanggil hero name
 print(hero1.name) # memanggil nama dari hero
 # contoh mamanggil stat dan umur
